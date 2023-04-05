f2ff5f5b27bf83c406414cc692e52fbb6bd86f28
Sam <uumasai@zenon.ai>
2023-04-05 17:21:59 +0530

    puppeterr-aws

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 const express = require('express');
-const { chromium } = require('playwright');
+// const { chromium } = require('playwright');
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+
+
+
+let chrome = {};
+let puppeteer;
+
+if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
+  chrome = require("chrome-aws-lambda");
+  puppeteer = require("puppeteer-core");
+} else {
+  puppeteer = require("puppeteer");
+}
+
+
 
 app.get('/', async (req, res) => {
   const url = req.query.url;
@@ -10,11 +25,23 @@ app.get('/', async (req, res) => {
   if (!url) {
     return res.status(400).send('Missing "url" parameter.');
   }
+  let options = {};
 
-  const browser = await chromium.launch();
-  const page = await browser.newPage();
+  if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
+    options = {
+      args: [...chrome.args, "--hide-scrollbars", "--disable-web-security"],
+      defaultViewport: chrome.defaultViewport,
+      executablePath: await chrome.executablePath,
+      headless: true,
+      ignoreHTTPSErrors: true,
+    };
+  }
+
+  
 
   try {
+    const browser = await puppeteer.launch(options);
+    const page = await browser.newPage();
     await page.goto(url, { waitUntil: 'networkidle' });
     const content = await page.content();
     res.send(content);
--- install-browsers.js
+++
@@ -1,6 +0,0 @@
-const { playwright } = require('playwright-core');
-const { installBrowsersWithProgressBar } = require('playwright/lib/install/installerWithProgressBar');
-
-(async () => {
-  await installBrowsersWithProgressBar(playwright);
-})();
